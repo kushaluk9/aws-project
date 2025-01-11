75f4e57fa7d6118f185fc9ee6af31265edfdadf5
kushaluk9 <kukawsuk@gmail.com>
2025-01-11 14:53:39 +0530

    updated aws project

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import server from "./app.js";
 import { PORT } from "./solution.js";
 
-server.listen(PORT ,'0.0.0.0', () => {
+server.listen(PORT , () => {
   console.log('Server is running and accessible externally on port 3000');
 })
